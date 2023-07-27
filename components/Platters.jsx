31ad22a459a5131927f63fd38578f621ee118c2f
codigoreactivo <jesusjhoel.aquino@outlook.com>
2023-07-27 12:47:36 -0500

    pl

--- components/Platters.jsx
+++ components/Platters.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Platters = () => {
   return (
     <div className='flex flex-col relative justify-center items-center h-screen'>
-        <h2 className='font-[BebasNeue-Regular] text-center leading-none flex-wrap text-[8rem] lg:text-[12rem] uppercase'>Party Platters?</h2>
+        <h2 className='font-[BebasNeue-Regular] text-center leading-none flex-wrap text-[4rem] lg:text-[12rem] uppercase'>Party Platters?</h2>
         <div className='w-[10rem] bg-[#67BFB1] text-center rounded-full uppercase font-[600] text-[#004857] hover:text-[#fff] py-4 px-6 z-20'><Link href='https://drive.google.com/file/d/1eZLEKbpLzA98rruakJqbMDMtnZFG3lpb/view?usp=sharing' target='_blank'>Togo Here</Link></div>
         <Image className='absolute right-0 top-0' src='/img/7.png' width={2500} height={200}></Image>
     </div>
